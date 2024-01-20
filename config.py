a50b68e504cd11453e537c9b9d24a1ed18ea3fda
Kristiyan Gostev <kristiyan.gostev@bosch.com>
2024-01-20 12:29:32 +0200

    Switch robot hostname
    Signed-off-by: Kristiyan Gostev <kristiyan.gostev@bosch.com>

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ mqtt_password = os.environ.get("LOCAL_MQTT_PASSWORD", "")
 
 device_id = os.getenv("DEVICE_ID", "demo:robot:1.0.0")
 
-robot_hostname = os.environ.get("ROBOT_HOSTNAME", "localhost")
+robot_hostname = os.environ.get("ROBOT_HOSTNAME", "kinovarobot")
 robot_port = os.environ.get("ROBOT_PORT", "10000")
 robot_port_udp = os.environ.get("ROBOT_PORT_UDP", "10001")
 robot_username = os.environ.get("ROBOT_USERNAME", "admin")
